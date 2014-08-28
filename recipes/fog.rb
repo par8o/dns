75add7345e9b11cce88ee9ee86dc59f9f3fcf273
Aaron Baer <aaron@hw-ops.com>
2014-08-27 23:12:12 -0700

    package name has changes to libxslt1-dev

--- recipes/fog.rb
+++ recipes/fog.rb
@@ -2,7 +2,7 @@ node.set[:build_essential][:compiletime] = true
 include_recipe 'build-essential'
 
 # Dependencies required by nokogiri (for fog)
-%w(libxslt-dev libxml2-dev).each do |pkg|
+%w(libxslt1-dev libxml2-dev).each do |pkg|
   c_pkg = package(pkg)
   c_pkg.run_action(:install)
 end
