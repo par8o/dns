14171945a193697332251a36faba27e96267f224
Aaron Baer <aaron@hw-ops.com>
2014-08-27 23:32:52 -0700

    need build-essential to install to correctly build fog

--- recipes/fog.rb
+++ recipes/fog.rb
@@ -1,15 +1,9 @@
-node.set[:build_essential][:compiletime] = true
-include_recipe 'build-essential'
-
 # Dependencies required by nokogiri (for fog)
-%w(libxslt1-dev libxml2-dev).each do |pkg|
+%w(build-essential libxslt1-dev libxml2-dev).each do |pkg|
   c_pkg = package(pkg)
   c_pkg.run_action(:install)
 end
 
-
-# TODO: Remove this once the gem_hell cookbook is ready to roll
 chef_gem "fog" do
   version node[:dns][:fog_version]
-  action :install
 end
